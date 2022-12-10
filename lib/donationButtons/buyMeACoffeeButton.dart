8baf62b76334a65a78e5a96e7ebc0d1d1f14cb5c
MrCsabaToth <csaba.toth.us@gmail.com>
2022-12-10 14:43:48 -0800

    Code lint: getting rid of unused imports + flutter format

--- lib/donationButtons/buyMeACoffeeButton.dart
+++ lib/donationButtons/buyMeACoffeeButton.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 ///Colors to choose from
--- lib/donationButtons/patreonButton.dart
+++ lib/donationButtons/patreonButton.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:simple_icons/simple_icons.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 ///Patreon support button
